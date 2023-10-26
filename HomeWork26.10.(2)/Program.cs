952526c7ef710f1be84a2c6140c61905646467b9
Void <Void@SWEETVOID1488>
2023-10-26 21:22:46 +0300

    Add project files.

---
+++ HomeWork26.10.(2)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the digit: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isPalindrom = IsPalindrome(number);
+        if (isPalindrom)
+        {
+            Console.WriteLine($"{number} is a palindrome. ");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is a not palindrome. ");
+
+        }
+
+        static bool IsPalindrome(int number)
+        {
+            int originalNumber = number;
+            int reversedNumber = 0;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                reversedNumber = reversedNumber * 10 + digit;
+                number = number / 10;
+            }
+            return originalNumber == reversedNumber;
+        }
+    }
+}
